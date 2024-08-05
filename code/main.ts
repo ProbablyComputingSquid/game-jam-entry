93cce44db33102d4373df47988d391e7d671d5a9
ComputingSquid <jwbolling001@gmail.com>
2024-08-05 13:47:30 -0500

    made everything work, fixed npm not working (my computer had problems) also added an enemy so that's cool it should move towards the player i think..

--- code/main.ts
+++ code/main.ts
@@ -1,28 +1,55 @@
 import kaboom from "kaboom"
 import "kaboom/global"
 
+const SPEED = 320;
+let enemyList = []
+
 // initialize context
 kaboom()
 
 // load assets
 loadSprite("bean", "sprites/bean.png")
+loadSprite("enemy", "sprites/unhappy.png")
+loadSprite("sword", "sprites/sword.png")
 
-// add a character to screen
+function addEnemy(enemyHealth = 1) {
+	const enemy = add([
+		sprite("enemy"),
+		pos(rand(0, width()), rand(0, height())),
+		area(),
+		body(),
+		health(enemyHealth),
+		"enemy",
+	])
+	enemy.move(enemy.pos.angle(player.pos), 200)
+	enemyList.push(enemy)
+
+}
+
+// add player to screen
 const player = add([
 	// list of components
 	sprite("bean"),
 	pos(80, 40),
 	area(),
+	body(),
+	health(8),
 ])
-
-// add a kaboom on mouse click
-onClick(() => {
-	addKaboom(mousePos())
-})
 
 
 // control stuff
 
-onKeyPress("up", () => {
-	
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
 })
+onKeyDown("left", () => {
+	player.move(-SPEED, 0)
+})
+onKeyDown("up", () => {
+	player.move(0, -SPEED)
+})
+onKeyDown("down", () => {
+	player.move(0, SPEED)
+})
+
+addEnemy();
